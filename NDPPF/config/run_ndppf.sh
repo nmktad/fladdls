798b10826a6425428db7bb97da6c61e291b3c34b
Masoud Baharlouei <masoud.baharlouei@lecnam.net>
2025-10-08 10:10:47 +0200

    Updated ntp, threshold curve

--- NDPPF/config/run_ndppf.sh
+++ NDPPF/config/run_ndppf.sh
@@ -17,6 +17,9 @@ echo "You should see this message after kafka instances are up ..."
 sleep 5
 
 echo "starting NDPPF_$1 post processing container ..."
+
+cp /app/confs/ndppf.py /network_data_preprocessing_function/
+
 cd /network_data_preprocessing_function
 echo "running: collected_cpu_topic_$1 preprocessed_cpu_topic_t_$1 preprocessed_cpu_topic_i_$1 ${IKAFKAIP}:${KAFKAPORT} ${EKAFKAIP}:${KAFKAPORT}"
 python3 ndppf.py polling cpu 100 collected_cpu_topic_$1 preprocessed_cpu_topic_t_$1 preprocessed_cpu_topic_i_$1 ${IKAFKAIP}:${KAFKAPORT} ${EKAFKAIP}:${KAFKAPORT}
